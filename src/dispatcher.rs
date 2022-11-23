3a6f286f4a17716b26345917d061a95325f241c1
blu3c0ral <blu3c0ral@protonmail.ch>
2022-11-22 23:17:23 -0800

    Added VehicleClient implemantation

--- src/dispatcher.rs
+++ src/dispatcher.rs
@@ -4,29 +4,69 @@
 /// The current thread will recieve orders from the feedback engine.
 /// See: Velocirustor.png
 
+use std::str::FromStr;
+use btleplug::api::BDAddr;
+
 use rust_powered_lego::lego::consts::{
     EndState,
-    Profile,
+    Profile, 
+    TechnicHubPorts,
 };
 
-#[allow(unused)]
-use tokio::sync::mpsc::{self, UnboundedSender, UnboundedReceiver};
+use rust_powered_lego::connection_manager::ConnectionManager;
+use rust_powered_lego::lego::message_parameters::StartupAndCompletionInfo;
+use rust_powered_lego::{
+    HubType, 
+    MotorType,
+    hub::Hub
+};
 
-use anyhow::{Result, Ok, bail};
+use tokio::runtime::Builder;
+use tokio::sync::mpsc::{
+    UnboundedSender, 
+    UnboundedReceiver,
+    self
+};
 
-use crate::VehicleSteeringAPI;
+use anyhow::{
+    Result, 
+    Ok, 
+    bail
+};
+
+use crate::{
+    VehicleSteeringAPI, 
+    VehicleMotorAPI
+};
+
+
+#[derive(Debug)]
+#[repr(u8)]
+pub enum VehicleSpecificCommands {
+    SteerByPosition             = 0x00,
+    SteerUntilStopped           = 0x01,
+    StopSteer                   = 0x02,
+
+    ActivateMotorUntilStopped   = 0x03,
+    StopMotor                   = 0x04,
+}
 
 // To save space and troubles, each argument is stored in its own vector.
 // Order is important.
 #[derive(Debug)]
 pub struct Command {
-    pub command_id: u8,
+    pub command_id: VehicleSpecificCommands,
     pub args_u8:    Option<Vec<u8>>,
     pub args_i8:    Option<Vec<i8>>,
     pub args_i32:   Option<Vec<i32>>,
     pub end_state:  Option<EndState>,
 }
 
+
+/// VehicleAPI is implemantation of the Vehicle<Motor, Steering>API trait to control
+/// a vehicle. The vehicle API is not alone - it needs a client that is a vehicle-specific.
+/// Here a lego motors are being used.
+/// See LegoVehicleDispatcher below for the implemantation for the lego motors specifically.
 pub struct VehicleAPI {
     event_tx: UnboundedSender<Command>,
 }
@@ -37,6 +77,30 @@ impl VehicleAPI {
             event_tx,
         }
     }
+
+    fn send_command(
+        &self,
+        command_id:     VehicleSpecificCommands, 
+        args_u8:        Option<Vec<u8>>,
+        args_i8:        Option<Vec<i8>>,
+        args_i32:       Option<Vec<i32>>,
+        end_state:      Option<EndState>,
+        command_name:   &str,
+    ) -> Result<()> {
+        let res = self.event_tx.send(
+            Command {
+                command_id, 
+                args_u8, 
+                args_i8, 
+                args_i32,
+                end_state,
+            }
+        );
+        if res.is_err() {
+            bail!("Error in {}: {:?}", command_name, res)
+        }
+        Ok(())
+    }
 }
 
 impl VehicleSteeringAPI for VehicleAPI {
@@ -45,44 +109,191 @@ impl VehicleSteeringAPI for VehicleAPI {
 
     // fn update_steering_motor_profile(profile_id: u8, profile: Self::MotorProfile) -> Result<()> {unimplemented!()}
     // fn steer_by_degree(degrees: i32, power: i8) -> Result<()> {unimplemented!()}
-    // fn steer_by_pos(abs_pos: i32, power: i8) -> Result<()> {unimplemented!()}
     
-    fn steer_until_stopped(&self, direction: i8, power: i8) -> Result<()> {
-        let res = self.event_tx.send(
-            Command {
-                command_id: VehicleSpecificCommands::SteerUntilStopped as u8, 
-                args_u8:    None, 
-                args_i8:    Some(vec![direction, power]), 
-                args_i32:   None,
-                end_state:  None,
-            }
-        );
-        if res.is_err() {
-            bail!("Error in steer_until_stopped: {:?}", res)
-        }
-        Ok(())
+    fn steer_by_pos(&self, abs_pos: i32) -> Result<()> {
+        self.send_command(
+            VehicleSpecificCommands::SteerByPosition, 
+            None, 
+            None, 
+            Some(vec![abs_pos]), 
+            None, 
+            "steer_by_pos",
+        )
+    }
+    
+    fn steer_until_stopped(&self, power: i8) -> Result<()> {
+        self.send_command(
+            VehicleSpecificCommands::SteerUntilStopped, 
+            None, 
+            Some(vec![power]),
+            None, 
+            None, 
+            "steer_until_stopped",
+        )
     }
     
     fn stop_steer(&self, end_state: Self::SteerEndState) -> Result<()> {
-        let res = self.event_tx.send(
-            Command {
-                command_id: VehicleSpecificCommands::StopSteer as u8, 
-                args_u8:    None, 
-                args_i8:    None, 
-                args_i32:   None,
-                end_state:  Some(end_state),
-            }
-        );
-        if res.is_err() {
-            bail!("Error in steer_until_stopped: {:?}", res)
-        }
-        Ok(())
+        self.send_command(
+            VehicleSpecificCommands::StopSteer, 
+            None, 
+            None, 
+            None, 
+            Some(end_state), 
+            "stop_steer",
+        )
     }
 }
 
 
-#[repr(u8)]
-enum VehicleSpecificCommands {
-    SteerUntilStopped   = 0x00,
-    StopSteer           = 0x01,
+impl VehicleMotorAPI for VehicleAPI {
+    type MotorProfile   = Profile;
+    type MotorEndState  = EndState;
+
+    // fn update_motor_profile(&self, profile_id: u8, profile: Self::MotorProfile) -> Result<()> {unimplemented!()}
+    
+    fn activate_motor_until_stopped(&self, power: i8) -> Result<()> {
+        self.send_command(
+            VehicleSpecificCommands::ActivateMotorUntilStopped, 
+            None, 
+            Some(vec![power]), 
+            None, 
+            None, 
+            "activate_motor_until_stopped",
+        )
+    }
+
+    fn stop_motor(&self, end_state: Self::MotorEndState) -> Result<()> {
+        self.send_command(
+            VehicleSpecificCommands::StopMotor, 
+            None, 
+            None, 
+            None, 
+            Some(end_state), 
+            "stop_motor",
+        )
+    }
+}
+
+
+
+
+/// LegoVehicleDispatcher is capable of holding a connection to lego hub and controling the motors connected to it.
+/// This is the "other" thread that is running tokio rt.
+/// run_dispatcher is the command event loop that is at the other end of VehicleAPI::send_command
+pub struct LegoVehicleDispatcher {
+    event_rx:   UnboundedReceiver<Command>,
+}
+
+impl LegoVehicleDispatcher {
+    pub fn new(event_rx: UnboundedReceiver<Command>) -> Self {
+        Self {
+            event_rx,
+        }
+    }
+
+    pub async fn run_dispatcher(&mut self, hub: impl HubType, steer_motor_port: TechnicHubPorts, motor_port: TechnicHubPorts) {
+        let motor = hub.get_motor(motor_port as u8).await.unwrap();
+        let steer_motor = hub.get_motor(steer_motor_port as u8).await.unwrap();
+
+        loop {
+            let command = self.event_rx.recv().await.unwrap();
+            
+            match command.command_id {
+                VehicleSpecificCommands::SteerByPosition => {
+                    goto_pos(&steer_motor, command.args_i32.unwrap()[0]).await;
+                },
+                VehicleSpecificCommands::SteerUntilStopped => {
+                    activate_motor(&steer_motor, command.args_i8.unwrap()[0]).await;
+                },
+                VehicleSpecificCommands::StopSteer => {
+                    stop_motor(&steer_motor, command.end_state.unwrap()).await;
+                },
+                VehicleSpecificCommands::ActivateMotorUntilStopped => {
+                    activate_motor(&motor, command.args_i8.unwrap()[0]).await;
+                },
+                VehicleSpecificCommands::StopMotor => {
+                    stop_motor(&motor, command.end_state.unwrap()).await;
+                },
+            }
+                
+        }
+    }
+}
+
+async fn activate_motor<T>(motor: &T, power: i8)
+where
+    T: MotorType 
+{
+    _ = motor.start_power(power, StartupAndCompletionInfo::ExecuteImmediatelyAndNoAction).await;
+}
+
+async fn stop_motor<T>(motor: &T, end_state: EndState)
+where
+    T: MotorType,
+{
+    _ = motor.stop_motor(
+        end_state, 
+        Profile::AccDec, 
+        StartupAndCompletionInfo::ExecuteImmediatelyAndNoAction
+    ).await;
+}
+
+async fn goto_pos<T>(motor: &T, abs_pos: i32)
+where
+    T: MotorType,
+{
+    _ = motor.set_abs_position(
+        abs_pos, 
+        StartupAndCompletionInfo::ExecuteImmediatelyAndNoAction
+    ).await;
+}
+
+
+
+
+pub struct LegoVehicleClient {
+    client: VehicleAPI,
+}
+
+impl LegoVehicleClient {
+    pub fn new(hub_address: &str, steer_motor_port: TechnicHubPorts, motor_port: TechnicHubPorts) -> Self {
+        let runtime = Builder::new_multi_thread()
+            .enable_all()
+            .build()
+            .unwrap();
+
+        let (event_tx, event_rx) = mpsc::unbounded_channel();
+
+        runtime.block_on(async {
+            let hub = get_hub(hub_address).await.unwrap();
+            runtime.spawn(async move {
+                LegoVehicleDispatcher::new(event_rx).run_dispatcher(hub, steer_motor_port, motor_port).await;
+            });
+        });
+
+        Self {
+            client: VehicleAPI::new(event_tx),
+        }
+    }
+
+    pub fn get_client(&self) -> &VehicleAPI {
+        &self.client
+    }
+
+}
+
+async fn get_hub(address:  &str) -> Result<Hub> {
+    
+    // Converting the MAC string to btleplug::api::BDAddr type
+    let address = BDAddr::from_str(address)?;
+
+    // The ConnectionManager connects stuff - so ask it for the hub...
+    let cm = ConnectionManager::new();
+
+    // It is possible to use the name of the hub or its MAC address. That's why it's Option<>
+    // Here, only address is implemented
+    let hub = cm.get_hub(None, Some(address), 5).await?;
+    
+    // Great! Let's get on with this...
+    Ok(hub)
 }
--- src/lib.rs
+++ src/lib.rs
@@ -15,8 +15,8 @@ pub trait VehicleSteeringAPI {
     
     fn update_steering_motor_profile(&self, profile_id: u8, profile: Self::MotorProfile) -> Result<()> {unimplemented!()}
     fn steer_by_degree(&self, degrees: i32, power: i8) -> Result<()> {unimplemented!()}
-    fn steer_by_pos(&self, abs_pos: i32, power: i8) -> Result<()> {unimplemented!()}
-    fn steer_until_stopped(&self, direction: i8, power: i8) -> Result<()> {unimplemented!()}
+    fn steer_by_pos(&self, abs_pos: i32) -> Result<()> {unimplemented!()}
+    fn steer_until_stopped(&self, power: i8) -> Result<()> {unimplemented!()}
     fn stop_steer(&self, end_state: Self::SteerEndState) -> Result<()> {unimplemented!()}
 }
 
@@ -26,6 +26,6 @@ pub trait VehicleMotorAPI {
     type MotorEndState;
 
     fn update_motor_profile(&self, profile_id: u8, profile: Self::MotorProfile) -> Result<()> {unimplemented!()}
-    fn activate_motor_until_stopped(&self, direction: u8, power: i8) -> Result<()> {unimplemented!()}
+    fn activate_motor_until_stopped(&self, power: i8) -> Result<()> {unimplemented!()}
     fn stop_motor(&self, end_state: Self::MotorEndState) -> Result<()> {unimplemented!()}
 }
